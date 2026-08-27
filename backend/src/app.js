dd7eeba4ee5b1c056da316141b44cd4cc398bd1d
AllisonE-code <allisonmeigsti@gmail.com>
2026-08-27 19:31:06 +0200

    Register functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const db = require('./config/db')
 const authRouter = require('./routes/auth-router.js')
 const postRouter = require('./routes/post-router.js')
-// const cors = require('cors')
+const cors = require('cors')
 const logger = require('./middleware/logger')
 const serverError = require('./middleware/server-error')
 const notFound = require('./middleware/not-found')
@@ -15,7 +15,7 @@ const authorization = require('./middleware/authorization')
 db()
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 app.use(logger)
 
 // Unprotected Routes
